3745040854f69ffdd9342ab08cd001518be584c7
Andre da Silva <andre-da.silva@outlook.com>
2021-12-03 16:53:39 +0100

    Add day 1 part one

---
+++ 2021/day1/part_1.py
@@ -0,0 +1,25 @@
+import os
+
+
+def parse_file(file_name: str) -> list[int]:
+    with open(file_name, "r") as f:
+        return list(map(lambda num: int(num), f.readlines()))
+
+
+def main():
+    nums = parse_file(file_name='1_input.txt')
+    counter = 0
+
+    for i in range(len(nums) - 1):
+        j = i + 1
+
+        before = nums[i]
+        after = nums[j]
+
+        if after > before:
+            counter += 1
+
+    print(f'total increases = {counter}, brought to you by python')
+
+if __name__ == "__main__":
+    main()
