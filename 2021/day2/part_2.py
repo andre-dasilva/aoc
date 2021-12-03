c60b830581c1040d68386d89da3d925fe236d8dd
Andre da Silva <andre-da.silva@outlook.com>
2021-12-04 00:09:58 +0100

    Add day 2 part two

---
+++ 2021/day2/part_2.py
@@ -0,0 +1,32 @@
+def parse_file(file_name: str) -> list[int]:
+    with open(file_name, "r") as f:
+         return list(map(lambda line: (line[0], int(line[1])),
+             map(lambda line: line.split(' '), f.readlines())
+         ))
+
+
+def main():
+    rows = parse_file(file_name='part_1_input.txt')
+
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for direction, number in rows:
+        match direction:
+            case "forward":
+                horizontal += number
+                depth += (aim * number)
+            case "down":
+                aim += number
+            case "up":
+                aim -= number
+
+    total = horizontal * depth
+
+    print(f'total = {total}, brought to you by python')
+
+
+
+if __name__ == "__main__":
+    main()
