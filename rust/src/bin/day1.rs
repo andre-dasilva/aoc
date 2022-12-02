f0669b8b30a464923b0482711f5309a02c851259
André da Silva <andre-da.silva@outlook.com>
2022-12-02 10:09:02 +0100

    Add day 1 :-)

--- rust/src/bin/day1.rs
+++ rust/src/bin/day1.rs
@@ -1,6 +1,25 @@
-fn main() {
-    let text = std::fs::read_to_string("./data/test.txt").unwrap();
-
-    print!("{}", text);
+fn part_1(total_calories_per_elf: &[usize]) {
+    println!("most calories single elf: {}", total_calories_per_elf.iter().max().unwrap())
 }
 
+fn part_2(total_calories_per_elf: &mut [usize]) {
+    total_calories_per_elf.sort();
+
+    println!("most calories top 3 elves: {}", total_calories_per_elf.iter().rev().take(3).sum::<usize>())
+}
+
+fn main() {
+    let text = std::fs::read_to_string("./data/day1/input.txt").unwrap();
+
+    let mut total_calories_per_elf: Vec<usize> = text
+        .split("\n\n")
+        .map(|s| {
+            s.split('\n')
+                .flat_map(|s| s.parse::<usize>())
+                .sum::<usize>()
+        })
+        .collect();
+
+    part_1(&total_calories_per_elf);
+    part_2(&mut total_calories_per_elf);
+}
