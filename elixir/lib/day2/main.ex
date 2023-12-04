7a3a4c391c131c3c2aa4568aa4e7f3109e9787be
André da Silva <andre-da.silva@outlook.com>
2023-12-04 15:31:42 +0100

    Add day2 part 1 of 2023 in elixir

--- elixir/lib/day2/main.ex
+++ elixir/lib/day2/main.ex
@@ -1,5 +1,64 @@
 defmodule Day2 do
-  def main do
-    :world
+  def part_1(input) do
+    # Prepare data
+    games =
+      File.stream!(input)
+      |> Stream.map(&String.trim/1)
+      |> Stream.map(fn line ->
+        [game_text, sets_text] =
+          line
+          |> String.split(":")
+          |> Enum.map(&String.trim/1)
+
+        [game_id] =
+          game_text
+          |> String.split(" ")
+          |> Enum.flat_map(fn text ->
+            case(Integer.parse(text)) do
+              {num, _} -> [num]
+              :error -> []
+            end
+          end)
+
+        sets =
+          sets_text
+          |> String.split(";")
+          |> Enum.map(&String.trim/1)
+          |> Enum.map(fn set ->
+            set
+            |> String.split(",")
+            |> Enum.map(&String.trim/1)
+            |> Enum.map(fn cube ->
+              [amount, color] =
+                cube
+                |> String.split(" ")
+
+              [String.to_atom(color), String.to_integer(amount)]
+            end)
+          end)
+
+        [game_id, sets]
+      end)
+      |> Enum.to_list()
+
+    # Search for solution in prepared data
+    Enum.filter(games, fn game ->
+      [game_id, sets] = game
+
+      Enum.all?(sets, fn set ->
+        Enum.all?(set, fn cube ->
+          case cube do
+            [:red, amount] -> amount <= 12
+            [:green, amount] -> amount <= 13
+            [:blue, amount] -> amount <= 14
+            _ -> true
+          end
+        end)
+      end)
+    end)
+    |> Enum.map(fn [game_id, _] -> game_id end)
+    |> Enum.sum()
   end
+
+  def part_2(input), do: nil
 end
--- elixir/test/day1/day1_test.exs
+++ elixir/test/day1/day1_test.exs
@@ -1,11 +1,13 @@
 defmodule Day1Test do
   use ExUnit.Case
 
+  @inputs_folder "test/day1"
+
   test "part 1" do
-    assert Day1.part_1("test/day1/part_1.txt") == 142
+    assert Day1.part_1("#{@inputs_folder}/part_1.txt") == 142
   end
 
   test "part 2" do
-    assert Day1.part_2("test/day1/part_2.txt") == 281
+    assert Day1.part_2("#{@inputs_folder}/part_2.txt") == 281
   end
 end
