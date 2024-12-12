2714153f5bf7b4fff8511b000a3e22c7c573f10e
André da Silva <andre-da.silva@outlook.com>
2024-12-12 11:14:43 +0100

    Add Day 2 part 2 :)

--- fsharp/aoc/Day2.fs
+++ fsharp/aoc/Day2.fs
@@ -6,44 +6,72 @@ type ReportState =
     | Safe
     | Unsafe
 
+let checkOrder lineOfNumbers orderPredicate =
+    Seq.pairwise lineOfNumbers |> Seq.forall orderPredicate
+
+let isPairBetweenRange lineOfNumbers min max =
+    Seq.pairwise lineOfNumbers
+    |> Seq.forall (fun (first, second) ->
+        let difference = abs (first - second)
+        difference >= min && difference <= max)
+
+let safeOrUnsafeLine lineOfNumbers =
+    let isAscending = checkOrder lineOfNumbers (fun (first, second) -> second >= first)
+    let isDescending = checkOrder lineOfNumbers (fun (first, second) -> second <= first)
+    let isBetweenRange = isPairBetweenRange lineOfNumbers 1 3
+
+    if (isAscending || isDescending) && isBetweenRange then
+        Safe
+    else
+        Unsafe
+
+let getLinesOfNumbers (input: string) =
+    input.Split("\n")
+    |> Seq.ofArray
+    |> Seq.map (fun line ->
+        line.Split(" ")
+        |> Array.filter (fun n -> String.IsNullOrEmpty(n) = false)
+        |> Array.map Int32.Parse)
+    |> Seq.filter (fun line -> line.Length > 0)
+
 let part_1 (input: string) =
-    let checkOrder lineOfNumbers orderPredicate =
-        Seq.pairwise lineOfNumbers |> Seq.forall orderPredicate
+    let linesOfNumbers = getLinesOfNumbers input
 
-    let isPairBetweenRange lineOfNumbers min max =
-        Seq.pairwise lineOfNumbers
-        |> Seq.forall (fun (first, second) ->
-            let difference = abs (first - second)
-            difference >= min && difference <= max)
+    linesOfNumbers
+    |> Seq.map safeOrUnsafeLine
+    |> Seq.sumBy (fun report ->
+        match report with
+        | Safe -> 1
+        | Unsafe -> 0)
 
-    let safeOrUnsafe lineOfNumbers =
-        let isAscending = checkOrder lineOfNumbers (fun (first, second) -> second >= first)
-        let isDescending = checkOrder lineOfNumbers (fun (first, second) -> second <= first)
-        let isBetweenRange = isPairBetweenRange lineOfNumbers 1 3
+let part_2 (input: string) =
+    let linesOfNumbers = getLinesOfNumbers input
 
-        if (isAscending || isDescending) && isBetweenRange then
-            Safe
+    let rec checkSafeOrUnsafeByRemovingOneLevel (line: int array) currentIndex =
+        if currentIndex <= line.Length - 1 then
+            let removedOneLevel = Array.removeAt currentIndex line
+
+            match safeOrUnsafeLine removedOneLevel with
+            | Safe ->
+                printfn "Safe: %A by removing the %A level, %A" line (currentIndex + 1) line[currentIndex]
+                Safe
+            | Unsafe -> checkSafeOrUnsafeByRemovingOneLevel line (currentIndex + 1)
         else
             Unsafe
 
-    let reports =
-        input.Split("\n")
-        |> Seq.ofArray
-        |> Seq.map (fun line ->
-            line.Split(" ")
-            |> Array.filter (fun n -> String.IsNullOrEmpty(n) = false)
-            |> Array.map Int32.Parse)
-        |> Seq.filter (fun line -> line.Length > 0)
-        |> Seq.map (fun line -> safeOrUnsafe line)
-
-
-    let totalSafeReports =
-        reports
-        |> Seq.sumBy (fun report ->
-            match report with
-            | Safe -> 1
-            | Unsafe -> 0)
-
-    totalSafeReports
-
-let part_2 (input: string) = 0
+    linesOfNumbers
+    |> Seq.map (fun line ->
+        let safeOrUnsafe = safeOrUnsafeLine line
+        (safeOrUnsafe, line))
+    |> Seq.map (
+        (fun (safeOrUnsafe, line) ->
+            match safeOrUnsafe with
+            | Safe ->
+                printfn "Safe: %A is safe without removing any level" line
+                Safe
+            | Unsafe -> checkSafeOrUnsafeByRemovingOneLevel line 0)
+    )
+    |> Seq.sumBy (fun report ->
+        match report with
+        | Safe -> 1
+        | Unsafe -> 0)
--- fsharp/aoc/Tests.fs
+++ fsharp/aoc/Tests.fs
@@ -35,7 +35,6 @@ let ``Day 1 - Part 2`` () =
     // let input = readFile "day1/input.txt"
     Assert.Equal((Day1.part_2 input), 31)
 
-
 [<Fact>]
 let ``Day 2 - Part 1`` () =
     let input =
@@ -48,3 +47,16 @@ let ``Day 2 - Part 1`` () =
 
     // let input = readFile "day2/input.txt"
     Assert.Equal((Day2.part_1 input), 2)
+
+[<Fact>]
+let ``Day 2 - Part 2`` () =
+    let input =
+        """7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9"""
+
+    // let input = readFile "day2/input.txt"
+    Assert.Equal((Day2.part_2 input), 4)
